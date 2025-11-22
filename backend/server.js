fa46700113065b8935b0b7ff953d525ba6e2b22a
Henrique Samuel Rosa <samuel_kuik@hotmail.com>
2025-11-22 00:49:45 +0000

    criado o servidor e adicionado o ESLINT

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+const PORT = 4000;
+
+app.get('/', (_req, res) => {
+    res.send('Servidor do TuneSpace está rodando');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
